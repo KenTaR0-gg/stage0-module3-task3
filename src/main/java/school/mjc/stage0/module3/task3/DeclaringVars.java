ee84da8b89104812dbd6d1c5c24d16572dc2a48a
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-22 04:20:53 +0300

    module 3. task 3.

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,10 +2,8 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        int c = 10;
-        System.out.println(c);
+        int a,b,c=10;
+        System.out.print(c);
 
     }
 }
